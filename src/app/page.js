f3b9e77965d93b214b805bb9af5c77302a12d418
ByulKim <kimbyul1225@gmail.com>
2022-11-19 17:08:21 +0900

    동적 라우팅

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -6,6 +6,12 @@ export default function Home() {
       <h2>
         Welcome!
       </h2>
+      <Image
+        src="/next.svg"
+        alt="정적이미지 넣기"
+        width={300}
+        height={100}
+      />
     </>
   )
 }
