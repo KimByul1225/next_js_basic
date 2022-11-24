93651f3c0e298726bdaf24ecdeeb211f4a6c280d
ByulKim <kimbyul1225@gmail.com>
2022-11-24 19:53:24 +0900

    환경변수 세팅 중

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default async function RootLayout({ children }) {
 
-  const res = await fetch("http://localhost:9999/topics", { cache: 'no-store' });
+  const res = await fetch(process.env.API_URL+"topics", { cache: 'no-store' });
   const topics = await res.json();
 
   return (
